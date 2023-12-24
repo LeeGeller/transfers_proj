b916e3ea6aaf5996efb36e01f3e5ea170ce26985
Kris <ssspers92@gmail.com>
2023-12-24 13:32:45 +0300

    create get_transfers_list() Get list with transfers from data file

--- src/Transfers.py
+++ src/Transfers.py
@@ -1,3 +1,6 @@
+import json
+
+
 class Transfer:
     def __init__(self):
         self.list_transfer = []
@@ -6,3 +9,13 @@ class Transfer:
         return (f"Return file with informstion\n"
                 f"about users transfers:\n"
                 f"{self.list_transfer}\n")
+
+    def get_transfers_list(self, data_path):
+        """
+        Get list with transfers from data file
+        :param data_path: path to data file
+        :return: list with user transfers
+        """
+        with open(data_path, 'r', encoding='UTF-8') as data_file:
+            self.list_transfer = json.load(data_file)
+        return self.list_transfer
