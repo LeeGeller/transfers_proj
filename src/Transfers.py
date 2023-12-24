2fe15f6a4385aafec2eb7c775fa4ed6f049c889b
Kris <ssspers92@gmail.com>
2023-12-24 13:31:10 +0300

    create class Transfer

---
+++ src/Transfers.py
@@ -0,0 +1,8 @@
+class Transfer:
+    def __init__(self):
+        self.list_transfer = []
+
+    def __repr__(self):
+        return (f"Return file with informstion\n"
+                f"about users transfers:\n"
+                f"{self.list_transfer}\n")
