d20abd5e4c6347be3b5bc20837ce1f537c756285
Kris <ssspers92@gmail.com>
2023-12-24 13:40:25 +0300

    add test_clean_transfers_list()

--- tests/test_transfers.py
+++ tests/test_transfers.py
@@ -4,3 +4,13 @@ from src.Transfers import Transfer
 
 def test_get_transfers_list():
     assert Transfer().get_transfers_list(DATA_TEST) == [1, 2, 3]
+
+
+def test_clean_transfers_list():
+    transfers = Transfer()
+    transfers.get_transfers_list(DATA_TEST)
+    assert Transfer().clean_transfers_list([{'state': 'EXECUTED'}, {}, {'state': 'CANCELED'}]) == [
+        {'state': 'EXECUTED'}]
+    assert Transfer().clean_transfers_list([{'state': 'EXECUTED'}, {}, {'state': 'CANCELED'}]) == [
+        {'state': 'EXECUTED'}]
+    assert len(transfers.list_transfer) == 3
