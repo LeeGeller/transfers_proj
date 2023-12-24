b0c73bda15d42bb399e5dd677ef5638e39de9e77
Kris <ssspers92@gmail.com>
2023-12-24 13:36:27 +0300

    add path to data

--- config.py
+++ config.py
@@ -2,3 +2,5 @@ import os.path
 
 DIR_ROOT = os.path.dirname(__file__)
 DATA = os.path.join(DIR_ROOT, 'data', 'operations.json')
+DATA_TEST = os.path.join(DIR_ROOT, 'tests', 'test_operations.json')
+DATA_TEST_LIST = os.path.join(DIR_ROOT, 'tests', 'test_data.json')
